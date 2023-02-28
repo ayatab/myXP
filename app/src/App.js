bc5a194b13071285176168ac4f0c9379380493e0
Justin Tham <justinctham@hotmail.com>
2023-02-27 23:18:19 -0800

    Cleaned up App.js

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
+import React from 'react';
+// import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
 
 function App() {
   return (
