02ee1be66f202120720f89e66d115e007807bb7a
Justin Tham <justinctham@hotmail.com>
2023-02-27 23:17:38 -0800

    Created react app

---
+++ app/src/App.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, Navigate, Outlet } from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+      <p>Hello!</p>
+    </div>
+  );
+}
+
+export default App;
