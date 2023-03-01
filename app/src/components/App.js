dc393c17f75d7836c4a88f64430fc5a043482b8a
Ayata Bernhardt <bernhardta01@gmail.com>
2023-02-28 21:55:38 -0800

    making profile card

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 // import ProfilePage from './ProfilePage.js';
 import { Routes, Route } from 'react-router-dom';
 import Home from './Home.js';
+import ProfilePage from './ProfilePage.js';
 
 function App(props) {
 
     return (
         <div>
             <Routes>
-                <Route index element={<Home />}></Route>
+                <Route index element={<ProfilePage />}></Route>
             </Routes>
 
         </div>
--- app/src/components/ProfilePage.js
+++ app/src/components/ProfilePage.js
@@ -3,8 +3,12 @@ import React from 'react';
 export default function ProfilePage(props) {
 
     return (
-        <div>
-            
+        <div className='container mt-5 p-5'>
+            <div className="col-lg-10 flex-column bg-white rounded">
+                <div className='profile-cover rounded-top' style={{ backgroundImage: 'url(profilebackground.png)' }}>
+                    
+                </div>
+            </div>
         </div>
     )
 }
