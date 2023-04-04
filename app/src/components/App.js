f74b27527b25cb6375eab9eb997c6b9c475d25a9
ayatab <42857860+ayatab@users.noreply.github.com>
2023-04-03 19:46:01 -0700

    making new pages to link

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Home from './Home.js';
 import Header from './Header.js';
 import ProfilePage from './ProfilePage.js';
+import LandingPage from './LandingPage.js';
 
 
 function App(props) {
@@ -14,7 +15,8 @@ function App(props) {
             <Header />
             <Routes>
                 <Route index element={<ProfilePage currentView={isExperience} />} ></Route>
-                
+                <Route index element={<LandingPage />} ></Route>
+                <Route path='/home' element={<Home />} ></Route>
             </Routes>
         </div>
     )
