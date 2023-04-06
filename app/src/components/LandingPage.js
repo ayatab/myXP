a76193e427a76e2c7fe063671cf61f9a0a896cd4
ayatab <42857860+ayatab@users.noreply.github.com>
2023-04-05 19:58:51 -0700

    added stuff to landing

--- app/src/components/LandingPage.js
+++ app/src/components/LandingPage.js
@@ -11,10 +11,24 @@ export default function LandingPage(props) {
 
                 </div>
                 <div className='text-end pe-5 pt-4 fs-4 text-light'>A centralized esports hub which allows gamers to showcase <br></br> their experience and make professional connections.</div>
-                <div className='d-flex flex-column justify-content-center row'>
-                    <div className='mt-4 col justify-content-center'>
+                <div className='d-flex text-center row'>
+                    <div className='mt-4 col'>
                         <img src='pics/favicon.png' className='landing-logo'></img>
-                        <p>Who is MyXP for?</p>
+                        <p className='landing-text'>Who is MyXP for?</p>
+                    </div>
+                    <div className='col mt-auto'>
+                        <p className='landing-text fw-normal'>Gamers</p>
+                    </div>
+                    <div className='col mt-auto'>
+                        <p className='landing-text fw-normal'>Recruiters</p>
+                    </div>
+                </div>
+                <div className='d-flex text-center row mt-5'>
+                    <div className='col mt-auto'>
+                        <p className='landing-text'>Promote your professional gaming career.</p>
+                    </div>
+                    <div className='col mt-auto'>
+                        <p className='landing-text fw-normal fs-6'>Through myXP, gamers are able to create their own customized profile which spotlights their gaming experience, allowing players from everywhere to share their talents and connect to a larger group of organizations, scouts, or teams. </p>
                     </div>
                 </div>
             </div>
