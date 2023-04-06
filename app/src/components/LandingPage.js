64cb882424ddce08fb4319572932d584187bdc1b
ayatab <42857860+ayatab@users.noreply.github.com>
2023-04-05 19:31:39 -0700

    added landing page stuff

--- app/src/components/LandingPage.js
+++ app/src/components/LandingPage.js
@@ -3,8 +3,21 @@ import React from 'react';
 export default function LandingPage(props) {
 
     return (
-        <div>
-            {/* <p>Hello</p> */}
+        <div className='landing-bg'>
+            <div className='container'>
+                <div className='d-flex'>
+                    <img src='pics/boys.png' className='landing-boys-pic img-fluid'></img>
+                    <div className='welcome-text justify-content-end'>Welcome <br></br> to MyXP</div>
+
+                </div>
+                <div className='text-end pe-5 pt-4 fs-4 text-light'>A centralized esports hub which allows gamers to showcase <br></br> their experience and make professional connections.</div>
+                <div className='d-flex flex-column justify-content-center row'>
+                    <div className='mt-4 col justify-content-center'>
+                        <img src='pics/favicon.png' className='landing-logo'></img>
+                        <p>Who is MyXP for?</p>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
